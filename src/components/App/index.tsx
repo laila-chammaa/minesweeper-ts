2fbe37f96ee78fd4604d8d2af6bb9b7c9f07602c
Laila Chamma'a <laylachammaa@gmail.com>
2020-07-29 08:58:44 -0400

    small fix

---
+++ src/components/App/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./App.scss";
+
+//functional component
+const App: React.FC = () => {
+  return <div className="App">Hello from App;</div>;
+};
+
+export default App;
